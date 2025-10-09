fe3b1e27679c90851b1f3f072bec2377c5bb44aa
Jocelyn <jocelyntan89@hotmail.com>
2025-10-09 14:16:24 +0200

    chore: add prettier

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,9 +1,11 @@
 import js from '@eslint/js'
 import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import pluginReact from 'eslint-plugin-react'
+import {
+  defineConfig,
+  globalIgnores,
+} from 'eslint/config'
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -12,12 +14,12 @@ export default defineConfig([
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
   },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
 ])
